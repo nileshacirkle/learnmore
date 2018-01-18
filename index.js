9835b70ee478cac0d66c329d6b8d29d1a2aca50c
PC-132\admin <vikesh.radhecreation@gmail.com>
2018-01-18 12:34:13 +0530

    fgfg

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
   
   
 var http = require('http');
-
+var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
 	console.log('successssss');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(3000);
+}).listen(port);
+
+
